651ad6ff29ad4787a0c4a4ba25992c7175e1c119
Emmyekwe <emmyekwe@gmail.com>
2021-12-12 02:45:06 +0100

    initial

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+// module.exports = {
+//   reactStrictMode: true,
+// }
+
+
 module.exports = {
-  reactStrictMode: true,
-}
+  images: {
+    domains: ["links.papareact.com", "image.tmdb.org"],
+  },
+};
+
